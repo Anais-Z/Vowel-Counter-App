07998e0792a7e282ea9d599d3599bbb0e1822e19
Anais Zulfequar <anaiszed447@gmail.com>
2021-08-29 18:00:49 -0400

    Gained access to the element and created an array of vowels.

---
+++ script.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector('input[name=enterText]')
+
+//declaring a variable to access the button
+const submit = document.querySelector('.submit')
+
+//creating the array of vowels
+const vowels = ['a','e','i','o','u']
+
+//creating the event listener for the button
+submit.addEventListener('click', () =>{
+    
+})
